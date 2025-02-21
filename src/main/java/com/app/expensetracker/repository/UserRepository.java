b4f7dee41ba91c898e0f8773c6242dfcd7f6b402
Georgios Pronoitis <gpronoitis@deloitte.gr>
2025-02-21 10:37:45 +0200

    dockerize spring boot app with mysql

--- src/main/java/com/app/expensetracker/repository/UserRepository.java
+++ src/main/java/com/app/expensetracker/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.app.expensetracker.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
